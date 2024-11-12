facb2a06c757daee74157f9b5f28bcbddf08317c
Michael Barton <brizgly@mail.ru>
2024-11-12 15:09:56 +0300

    programm ver 0.1

---
+++ BankAccount/BankAccount/BankAccount/BankAccount.cpp
@@ -0,0 +1,21 @@
+﻿
+#include "Bank.h"
+/// <summary>
+/// programms 
+/// use methods show balance
+/// use methods set money
+/// methods withdraw money
+/// </summary>
+/// <returns></returns>
+int main()
+{
+	float money;
+	std::cout << "Enter the top-up amount ->  ";
+	std::cin >> money;
+	std::cout << "You have replenished the amount: " << money <<"$"<< std::endl;
+	Bank a1;
+	a1.setAmount(money);
+	a1.getInfo();
+	a1.Withdraw();
+	
+}
